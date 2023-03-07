bd522827fe0a0fd7d3e860071523fa68670ccfa3
AndriyUKRAINE <kaskiwandriy@gmail.com>
2023-03-07 17:17:34 +0200

    lab2Finish

---
+++ page1.js
@@ -0,0 +1,19 @@
+function displayPhrase()
+{
+    document.getElementById("demo").innerHTML = 'I wont to do something';
+}
+
+function restart()
+{
+    document.getElementById("demo").innerHTML = 'Жми на кнопку ';
+}
+
+function changecolorB()
+{
+    document.getElementById("color").style.backgroundColor = "blue";
+}
+
+function changecolorY()
+{
+    document.getElementById("color").style.backgroundColor = "yellow";
+}
